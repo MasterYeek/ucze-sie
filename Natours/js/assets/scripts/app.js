b0bb343d531c777ae578a74b7db4f04a8ce658f3
Tomasz Świerczek <bogdanos18@gmail.com>
2023-04-05 12:29:48 +0200

    JS

---
+++ Natours/js/assets/scripts/app.js
@@ -0,0 +1,12 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+//'(' + defaultResult + ' + 10) + 3 / 2 -1';
+
+let calculationDescription = `(${defaultResult} + 10) + 3 / 2 -1`;
+
+
+outputResult(currentResult, calculationDescription);
