91668fc3730a6e7741c42396ab435538e3e9dd07
Tomasz Świerczek <bogdanos18@gmail.com>
2023-04-13 21:32:45 +0200

    js jonas

---
+++ Natours/JS Jonas/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+
+48 + 21 + 120 - 2;
+console.log(40 + 8 + 23 - 10);
+
+console.log("TOM");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+
+let myCurrentJob = "Teacher";
+
+console.log(myCurrentJob);
---
+++ Natours/JS Jonas/tasks.js
@@ -0,0 +1,9 @@
+let country = "Poland";
+
+let continet = "Europe";
+
+let population = "42mil";
+
+console.log(country);
+console.log(continet);
+console.log(population);
