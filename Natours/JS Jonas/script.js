a1e9a32e8c58d3c1d949d044b573367ffae64d54
Tomasz Świerczek <bogdanos18@gmail.com>
2023-04-19 13:10:44 +0200

    js jonas

--- Natours/JS Jonas/script.js
+++ Natours/JS Jonas/script.js
@@ -1,6 +1,8 @@
 let js = "amazing";
 
+/*
 48 + 21 + 120 - 2;
+
 console.log(40 + 8 + 23 - 10);
 
 console.log("TOM");
@@ -11,8 +13,54 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+
 let myFirstJob = "Programmer";
 
 let myCurrentJob = "Teacher";
 
 console.log(myCurrentJob);
+
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+//console.log(typeof true);
+console.log(typeof javascriptisfun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptisfun = "Yes";
+console.log(typeof javascriptisfun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+*/
+
+//math opeartors
+const now = 2037;
+const agetom = now - 1992;
+const agemaja = now - 2018;
+console.log(agetom, agemaja);
+
+console.log(agetom * 2, agemaja / 10);
+
+const firstname = "Tom";
+const lastname = "swierczek";
+console.log(firstname + " " + lastname);
+
+let x = 10 + 5;
+x += 10; // x = x+ 10 = 25
+x += 4; // x = x * 4 = 100
+x++; /// x=x+1
+x--;
+x--;
+console.log(x);
+
+//comparison operators
+
+console.log(agetom > agemaja); // > < >= <=
+console.log(agemaja >= 18);
--- Natours/JS Jonas/tasks.js
+++ Natours/JS Jonas/tasks.js
@@ -1,3 +1,4 @@
+/*
 let country = "Poland";
 
 let continet = "Europe";
@@ -7,3 +8,23 @@ let population = "42mil";
 console.log(country);
 console.log(continet);
 console.log(population);
+
+
+
+let isISland = false;
+console.log(typeof isISland);
+
+let language
+
+console.log(typeof isISland);
+
+console.log(typeof population);
+
+console.log(typeof language);
+
+
+language = "Polski";
+const country = "Poland";
+const nation = "Europe";
+const isIslad = false;
+*/
