0ac4745063cc77e8aff7e3d8b2f1a6bd2ccaebb6
Tomasz Świerczek <bogdanos18@gmail.com>
2023-05-05 13:01:52 +0200

    js jonas

--- Natours/JS Jonas/fundamentals p2/script.js
+++ Natours/JS Jonas/fundamentals p2/script.js
@@ -1,12 +1,11 @@
 "use strict";
-/*
 
+/*
 let hasDriverslicense = false;
 const passTest = true;
 
 if (passTest) hasDriverslicense = true;
 if (hasDriverslicense) console.log("I can drive");
-*/
 
 function logger() {
   console.log(` MY name is tomas`);
@@ -24,3 +23,74 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+
+//Functin declaration
+function calcAge1(birthyear) {
+  return 2023 - birthyear;
+}
+
+const age1 = calcAge1(1993);
+console.log(age1);
+
+// function expression
+
+const calcage2 = function (birthyear) {
+  return 2023 - birthyear;
+};
+
+const age2 = calcage2(1993);
+console.log(age1, age2);
+
+
+function calcAge1(birthyear) {
+  return 2023 - birthyear;
+}
+
+const calcAge3 = (birthyear) => 2023 - birthyear;
+const age3 = calcAge3(1993);
+console.log(age3);
+
+const yearsuntilrretirement = (birthyear, firstname) => {
+  const age = 2023 - birthyear;
+  const retirement = 65 - age;
+  return `${firstname} retires in ${retirement}`;
+};
+
+console.log(yearsuntilrretirement(1991, "Tom"));
+
+
+function cutfruitpieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applepieces = cutfruitpieces(apples);
+  const orangepieces = cutfruitpieces(oranges);
+
+  const juice = `Juice with ${applepieces} apples and ${orangepieces} 
+oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
+
+function calcAge1(birthyear) {
+  return 2023 - birthyear;
+}
+
+function yearsuntilretirement(birthyear, firstname) {
+  const age = calcAge1(birthyear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log("he needs more");
+    return retirement;
+  } else {
+    console.log(`${firstname} has already retired`);
+    return -1;
+  }
+}
+console.log(yearsuntilretirement(1993, "Tom"));
+console.log(yearsuntilretirement(1950, "mat"));
--- Natours/JS Jonas/fundamentals p2/tasks.js
+++ Natours/JS Jonas/fundamentals p2/tasks.js
@@ -1,3 +1,4 @@
+/*
 function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} people and its capital city is ${capitalCity}`;
 }
@@ -8,3 +9,61 @@ const describe2 = describeCountry("Niemcy", 56, "Berlin");
 
 const describe3 = describeCountry("Italy", 44, "Rzym");
 console.log(describe, describe2, describe3);
+
+
+function percentageofWorld(population) {
+  return (population / 7900) * 100;
+}
+
+const china = percentageofWorld(1441);
+const poland = percentageofWorld(42);
+const germany = percentageofWorld(35);
+
+console.log(china, poland, germany);
+
+const percentageofWorld1 = function (population) {
+  return (population / 7900) * 100;
+};
+const china2 = percentageofWorld1(1441);
+console.log(china2);
+
+
+
+const percentageofWorld = (population) => (population / 7900) * 100;
+
+const china = percentageofWorld(1441);
+
+console.log(china);
+
+
+function percentageofWorld(population) {
+  return (population / 7900) * 100;
+}
+
+const china = percentageofWorld(1441);
+const poland = percentageofWorld(42);
+const germany = percentageofWorld(35);
+
+function describePopulation(country, population) {
+  const percentage = percentageofWorld(population);
+
+  return `${country} has ${population} people, which is about ${percentage} of the world `;
+}
+
+console.log(describePopulation("Portugal", 10));
+*/
+
+
+const DolphinsAverage = (calcaverage) => (44 + 23 + 71) / 3
+const Koalas = (calcaverage) => (85 + 54 + 41) / 3
+
+function checkwinner (avgdolphins, avgkoalas)
+const scoredol = DolphinsAverage(avgdolphins);
+const scorekoal = Koalas(avgkoalas);
+
+if (scoredol > scorekoal){
+  console.log( `Dolphins win (${avgdolphins} vs ${avgkoalas} )` );
+  return scoredol  
+}else{
+  console.log(`Koalas win ${avgkoalas} vs ${avgdolphins}` );
+}
