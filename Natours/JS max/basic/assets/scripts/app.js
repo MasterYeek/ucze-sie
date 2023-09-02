88d5911233d1883876dacdc8b1682b1726c1f229
Tomasz Świerczek <bogdanos18@gmail.com>
2023-09-02 16:49:32 +0200

    js max

--- Natours/JS max/basic/assets/scripts/app.js
+++ Natours/JS max/basic/assets/scripts/app.js
@@ -1,4 +1,4 @@
-const defaultResult = 2;
+const defaultResult = 0;
 
 let currentResult = defaultResult;
 
@@ -6,17 +6,49 @@ let currentResult = defaultResult;
 //   const result = number1 + number2;
 //   return result;
 // }
+function GetUserInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, ResultBeforeCalc, calcNumber) {
+  const calcDescription = `${ResultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
 function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, "");
+  const enterNumber = GetUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + parseInt(enterNumber);
+  createAndWriteOutput("+", initialResult, enterNumber);
 }
-currentResult = add(1, 2);
-add(5, 5);
 
+function subtract() {
+  const enterNumber = GetUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - parseInt(enterNumber);
+  createAndWriteOutput("-", initialResult, enterNumber);
+}
+function multiply() {
+  const enterNumber = GetUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * parseInt(enterNumber);
+  createAndWriteOutput("*", initialResult, enterNumber);
+}
+
+function divide() {
+  const enterNumber = GetUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / parseInt(enterNumber);
+  createAndWriteOutput("/", initialResult, enterNumber);
+}
 //currentResult = currentResult + 10 + 10 * 3;
 
 addBtn.addEventListener("click", add);
+
+subtractBtn.addEventListener("click", subtract);
+
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 
 //let calculationDescription = `(${defaultResult} + 10) + 10 * 3`;
 // let errorMessage = "An error occurred";
