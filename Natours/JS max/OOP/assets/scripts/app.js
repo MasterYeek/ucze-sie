3758c0e372566218cb79dafbb11b87c1d127024e
Tomasz Świerczek <bogdanos18@gmail.com>
2023-09-16 14:39:49 +0200

    js max

--- Natours/JS max/OOP/assets/scripts/app.js
+++ Natours/JS max/OOP/assets/scripts/app.js
@@ -1,41 +1,67 @@
-const productList = {
-  products: [
-    {
-      title: "A pillow",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNJ8f51A2fJg4EPvVpEMJn1Lk9EO6EAdAovQHUWP61eHMQXCJ4A9FNuzmN1AFnSPJnSYQ&usqp=CAU",
-      price: 19.99,
-      description: "a soft pillow!",
-    },
-    {
-      title: "A pillow",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9fuvdEQsgAlsoL80iJf0O4TdxmhbQFX6JaDwDkubhwpbAhxAMxvhXbbbOQrzfZ1-bOxA&usqp=CAU",
-      price: 89.99,
-      description: "a carpiet which u might like ",
-    },
-  ],
+class Product {
+  // title = "DEFAULT";
+  // imageUrl;
+  // description;
+  // price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+      <img src="${this.product.imageUrl}" alt="${this.product.title}">
+      <div class="product-item__content">
+      <h2>${this.product.title}</h2>
+      <h3>\$${this.product.price}</h3>
+      <p>${this.product.description}</p>
+      <button>Add to Cart</button>
+      </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "A pillow",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNJ8f51A2fJg4EPvVpEMJn1Lk9EO6EAdAovQHUWP61eHMQXCJ4A9FNuzmN1AFnSPJnSYQ&usqp=CAU",
+      "a soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNJ8f51A2fJg4EPvVpEMJn1Lk9EO6EAdAovQHUWP61eHMQXCJ4A9FNuzmN1AFnSPJnSYQ&usqp=CAU",
+      "a carpiet which u might like ",
+      89.99
+    ),
+  ];
+  constructor() {}
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-      <div>
-      <img src="${prod.imageUrl}" alt="${prod.title}">
-      <div class="product-item__content">
-      <h2>${prod.title}</h2>
-      <h3>\$${prod.price}</h3>
-      <p>${prod.description}</p>
-      <button>Add to Cart</button>
-      </div>
-      </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(productList);
-  },
-};
+    renderHook.append(prodList);
+  }
+}
+const productList = new ProductList();
 productList.render();
